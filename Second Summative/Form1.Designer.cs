d9ec22f81b540526f8fa8afdcf15986edfe093c1
addiepant <aadhpant936@ed.amdsb.ca>
2016-10-13 12:46:39 -0400

    Add project files.

---
+++ Second Summative/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Second_Summative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkoutLabel = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Gabriola", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(26, -18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 68);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Too-Faced";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(41, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Custom Makeup Pallete";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 171);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // checkoutLabel
+            // 
+            this.checkoutLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.checkoutLabel.ForeColor = System.Drawing.SystemColors.Control;
+            this.checkoutLabel.Location = new System.Drawing.Point(73, 304);
+            this.checkoutLabel.Name = "checkoutLabel";
+            this.checkoutLabel.Size = new System.Drawing.Size(75, 23);
+            this.checkoutLabel.TabIndex = 4;
+            this.checkoutLabel.Text = "CheckOut";
+            this.checkoutLabel.UseVisualStyleBackColor = false;
+            this.checkoutLabel.Click += new System.EventHandler(this.checkoutLabel_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(77, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 14);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Snow Angel";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(148, 126);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(28, 20);
+            this.textBoxInput.TabIndex = 6;
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.AutoSize = true;
+            this.outputLabel.BackColor = System.Drawing.Color.Transparent;
+            this.outputLabel.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputLabel.Location = new System.Drawing.Point(70, 289);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(0, 13);
+            this.outputLabel.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(235, 328);
+            this.Controls.Add(this.outputLabel);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkoutLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button checkoutLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Label outputLabel;
+    }
+}
+
---
+++ Second Summative/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Second_Summative
+{
+    public partial class Form1 : Form
+    {
+        const double SNOW_ANGEL_COST = 10.50;
+        double price;
+        //int amount = 0;
+        int snowAngel = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkoutLabel_Click(object sender, EventArgs e)
+        {
+            snowAngel = Convert.ToInt16(textBoxInput.Text);
+
+            price = snowAngel * SNOW_ANGEL_COST;
+
+            outputLabel.Text = "Your Total comes to" + price.ToString("$0.00");
+        }
+    }
+}
